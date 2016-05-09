f911d268a2ba70165029983c6f65125435dd1ef9
milafrerichs <mila.frerichs@gmail.com>
2016-05-09 17:37:29 +0200

    Remove coffee script references

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -12,15 +12,13 @@ module.exports = function ( karma ) {
       <% scripts.forEach( function ( file ) { %>'<%= file %>',
       <% }); %>
       'src/**/*.js',
-      'src/**/*.coffee',
     ],
     exclude: [
       'src/assets/**/*.js'
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-phantomjs-launcher', 'karma-coffee-preprocessor', 'karma-coverage' ],
+    plugins: [ 'karma-jasmine', 'karma-phantomjs-launcher', 'karma-coverage' ],
     preprocessors: {
-      '**/*.coffee': 'coffee',
       'src/**/*.js': ['coverage']
     },
 
@@ -67,4 +65,3 @@ module.exports = function ( karma ) {
     ]
   });
 };
-

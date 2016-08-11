ff28ec2b0c60b5759c23b6a28ab71b76552c37e0
Ariel Nuñez <ingenieroariel@gmail.com>
2016-08-11 09:40:36 -0500

    Added grunt connect:serve task

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,6 +36,18 @@ module.exports = function ( grunt ) {
     pkg: grunt.file.readJSON("package.json"),
 
     /**
+     * Connect server for development
+     */
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: 'build'
+        }
+      }
+    },
+
+    /**
      * The banner is the comment that is placed at the top of our compiled
      * source files. It is first processed as a Grunt template, where the `<%=`
      * pairs are evaluated based on this very configuration object.

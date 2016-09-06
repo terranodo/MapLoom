80f34c95be3cab361ea7d67dc990dc5bdcc986a9
Lennin Suescun <lenninlasd@gmail.com>
2016-09-06 13:02:13 +0200

    Map preview on cart (#73)
    * Map preview everything on the cart (wip)
    
    * Map preview on the cart: fixing errors
    
    * When the mouse leave the row the map preview is removed
    
    * Rename function for a more semantic description
    
    * commit to reset test

--- src/common/addlayers/RegistryLayersDirective.js
+++ src/common/addlayers/RegistryLayersDirective.js
@@ -196,9 +196,14 @@
               }
             }
 
+            scope.addLayersAndMapPreviewToCart = function(layerConfig) {
+              scope.addToCart(layerConfig);
+              addCartLayersInMapPreview(layerConfig);
+            };
+
             scope.selectRow = function(layerConfig) {
               scope.selectedLayer = layerConfig;
-              scope.addToCart(layerConfig);
+              scope.addLayersAndMapPreviewToCart(layerConfig);
             };
 
             var addLayer = function(layerConfig) {
@@ -235,6 +240,10 @@
                 layer,
                 bboxLayer
               ];
+            };
+
+            scope.cleanMapPreview = function() {
+              scope.previewLayers = [];
             };
 
             scope.addToCart = function(layerConfig) {
@@ -282,6 +291,15 @@
 
             // load saved registry layers if they exist
             scope.addRegistryLayersFromSavedMap();
+
+            function addCartLayersInMapPreview(layerConfig) {
+              layerConfig.CRS = ['EPSG:4326'];
+              var layer = mapService.createLayerWithFullConfig(layerConfig, scope.currentServerId);
+              layer.layerId = layerConfig.layerId;
+              scope.previewLayers = [
+                layer
+              ];
+            }
           }
         };
       }
